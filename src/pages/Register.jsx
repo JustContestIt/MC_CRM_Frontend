0b80d2218d827e56c62619b3c66d55e5257fd091
JustContestIt <fantomasqwe17@gmail.com>
2022-09-29 21:26:58 +0300

    Remade Register page to form

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -5,6 +5,7 @@ import { BsInfoCircle } from "react-icons/bs"
 import axios from '../http/index';
 import { Navigate } from "react-router-dom";
 import Icon from "../components/UI/Icon/Icon";
+import cl from "../styles/Form.module.css"
 
 const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
@@ -25,6 +26,8 @@ const Register = () => {
     const [matchPwd, setMatchPwd] = useState('');
     const [validMatch, setValidMatch] = useState(false);
     const [matchFocus, setMatchFocus] = useState(false);
+
+    const [role, setRole] = useState(1)
 
     const [errMsg, setErrMsg] = useState('');
     const [success, setSuccess] = useState(false);
@@ -84,91 +87,128 @@ const Register = () => {
             {success ? (
                 <Navigate to='/'/>
             ) : (
-                <div>
-                    <p ref={errRef} className={errMsg ? "errMsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
-                    <h1>Регистрация</h1>
-                    <form onSubmit={handleSubmit}>
-                        <label htmlFor="username">
-                            Имя пользователя:
-                            <Icon icon={<AiOutlineCheck/>} classes={validName ? "valid" : "hide"}/>
-                            <Icon icon={<FaTimes/>} classes={validName ? "valid" : "hide"}/>
-                        </label>
-                        <input
-                            type="text"
-                            id="username"
-                            ref={userRef}
-                            autoComplete="off"
-                            onChange={(e) => setUser(e.target.value)}
-                            value={user}
-                            required
-                            aria-invalid={validName ? "false" : "true"}
-                            aria-describedby="uidNote"
-                            onFocus={() => setUserFocus(true)}
-                            onBlur={() => setUserFocus(false)}
-                        />
-                        <p id="uidNote" className={userFocus && user && !validName ? "instructions" : "offscreen"}>
-                            <Icon icon={<BsInfoCircle/>}/>
-                            от 4 до 24 символов.<br />
-                            Должен начинаться с буквы.<br />
-                            Допускаются буквы, цифры, символы подчеркивания, дефисы.
-                        </p>
-                        <label htmlFor="password">
-                            Пароль:
-                            <Icon icon={<AiOutlineCheck/>} classes={validName ? "valid" : "hide"}/>
-                            <Icon icon={<FaTimes/>} classes={validName ? "valid" : "hide"}/>
-                        </label>
-                        <input
-                            type="password"
-                            id="password"
-                            onChange={(e) => setPwd(e.target.value)}
-                            value={pwd}
-                            required
-                            aria-invalid={validPwd ? "false" : "true"}
-                            aria-describedby="pwdNote"
-                            onFocus={() => setPwdFocus(true)}
-                            onBlur={() => setPwdFocus(false)}
-                        />
-                        <p id="pwdNote" className={pwdFocus && !validPwd ? "instructions" : "offscreen"}>
-                            <Icon icon={<BsInfoCircle/>}/>
-                            от 8 до 24 символов.<br />
-                            Должен содержать заглавные и строчные буквы, цифру и специальный символ.<br />
-                            Разрешенные специальные символы:
-                            <span aria-label="exclamation mark">!</span>
-                            <span aria-label="at symbol">@</span>
-                            <span aria-label="hashtag">#</span>
-                            <span aria-label="dollar sign">$</span>
-                            <span aria-label="percent">%</span>
-                        </p>
+                <div className='overflowHide'>
+                    <div className='form-container mx-auto'>
+                        <div className='w-100'>
+                            <p ref={errRef} className={errMsg ? cl.errMsg : cl.offscreen} aria-live="assertive">{errMsg}</p>
+                            <h2 className='text-center mb-4'>Регистрация пользователя</h2>
+                            <br/>
+                            <form onSubmit={handleSubmit} className='d-flex justify-content-center flex-column'>
+                                <div className="form-group mb-2 text-start">
+                                    <label htmlFor="username" className='form-label'>
+                                        Имя пользователя:
+                                        {/*<Icon icon={<AiOutlineCheck/>} classes={validName ? cl.valid : cl.hide}/>*/}
+                                        {/*<Icon icon={<FaTimes/>} classes={!validName ? cl.valid : cl.hide}/>*/}
+                                    </label>
+                                    <input
+                                        type="text"
+                                        id="username"
+                                        ref={userRef}
+                                        autoComplete="off"
+                                        className='form-control'
+                                        onChange={(e) => setUser(e.target.value)}
+                                        value={user}
+                                        required
+                                        aria-invalid={validName ? "false" : "true"}
+                                        aria-describedby="uidNote"
+                                        onFocus={() => setUserFocus(true)}
+                                        onBlur={() => setUserFocus(false)}
+                                    />
+                                    <p
+                                        id="uidNote"
+                                        className={userFocus && user && !validName ? cl.instructions : cl.offscreen
+                                            + ' form-text'}
+                                    >
+                                        <Icon icon={<BsInfoCircle/>}/>
+                                        от 4 до 24 символов.<br />
+                                        Должен начинаться с буквы.<br />
+                                        Допускаются буквы, цифры, символы подчеркивания, дефисы.
+                                    </p>
+                                </div>
 
+                                <div className="form-group mb-2 text-start">
+                                    <label htmlFor="password" className='form-label'>
+                                        Пароль:
+                                        {/*<Icon icon={<AiOutlineCheck/>} classes={validName ? cl.valid : cl.hide}/>*/}
+                                        {/*<Icon icon={<FaTimes/>} classes={validName ? cl.valid : cl.hide}/>*/}
+                                    </label>
+                                    <input
+                                        type="password"
+                                        id="password"
+                                        className='form-control'
+                                        onChange={(e) => setPwd(e.target.value)}
+                                        value={pwd}
+                                        required
+                                        aria-invalid={validPwd ? "false" : "true"}
+                                        aria-describedby="pwdNote"
+                                        onFocus={() => setPwdFocus(true)}
+                                        onBlur={() => setPwdFocus(false)}
+                                    />
+                                    <p
+                                        id="pwdNote"
+                                        className={pwdFocus && !validPwd ? cl.instructions : cl.offscreen
+                                            + ' form-text'}>
+                                        <Icon icon={<BsInfoCircle/>}/>
+                                        от 8 до 24 символов.<br />
+                                        Должен содержать заглавные и строчные буквы, цифру и специальный символ.<br />
+                                        Разрешенные специальные символы:
+                                        <span aria-label="exclamation mark">!</span>
+                                        <span aria-label="at symbol">@</span>
+                                        <span aria-label="hashtag">#</span>
+                                        <span aria-label="dollar sign">$</span>
+                                        <span aria-label="percent">%</span>
+                                    </p>
+                                </div>
 
-                        <label htmlFor="confirm_pwd">
-                            Подтвердите пароль:
-                            <Icon icon={<AiOutlineCheck/>} classes={validName ? "valid" : "hide"}/>
-                            <Icon icon={<FaTimes/>} classes={validName ? "valid" : "hide"}/>
-                        </label>
-                        <input
-                            type="password"
-                            id="confirm_pwd"
-                            onChange={(e) => setMatchPwd(e.target.value)}
-                            value={matchPwd}
-                            required
-                            aria-invalid={validMatch ? "false" : "true"}
-                            aria-describedby="confirmNote"
-                            onFocus={() => setMatchFocus(true)}
-                            onBlur={() => setMatchFocus(false)}
-                        />
-                        <p id="confirmNote" className={matchFocus && !validMatch ? "instructions" : "offscreen"}>
-                            <Icon icon={<BsInfoCircle/>}/>
-                            Должен соответствовать первому полю ввода пароля.
-                        </p>
+                                <div className="form-group mb-3 text-start">
+                                    <label htmlFor="confirm_pwd" className='form-label'>
+                                        Подтвердите пароль:
+                                        {/*<Icon icon={<AiOutlineCheck/>} classes={validName ? cl.valid : cl.hide}/>*/}
+                                        {/*<Icon icon={<FaTimes/>} classes={validName ? cl.valid : cl.hide}/>*/}
+                                    </label>
+                                    <input
+                                        type="password"
+                                        id="confirm_pwd"
+                                        className='form-control'
+                                        onChange={(e) => setMatchPwd(e.target.value)}
+                                        value={matchPwd}
+                                        required
+                                        aria-invalid={validMatch ? "false" : "true"}
+                                        aria-describedby="confirmNote"
+                                        onFocus={() => setMatchFocus(true)}
+                                        onBlur={() => setMatchFocus(false)}
+                                    />
+                                    <p
+                                        id="confirmNote"
+                                        className={matchFocus && !validMatch ? cl.instructions : cl.offscreen
+                                            + ' form-text'}>
+                                        <Icon icon={<BsInfoCircle/>}/>
+                                        Должен соответствовать первому полю ввода пароля.
+                                    </p>
+                                    <select
+                                        value={role}
+                                        onChange={(e) => setRole(parseInt(e.target.value, 10))}
+                                        className='form-select mt-2'
+                                    >
+                                        <option value={1}>Студент</option>
+                                        <option value={2}>Тренер</option>
+                                        <option value={3}>Админ</option>
+                                    </select>
+                                </div>
 
-                        <button
-                            className={'btn btn-primary'}
-                            disabled={!validName || !validPwd || !validMatch}
-                        >
-                            Зарегистрироваться
-                        </button>
-                    </form>
+                                <br/>
+                                <br/>
+                                <div className="d-inline-block text-center">
+                                <button
+                                    className='btn btn-primary '
+                                    disabled={!validName || !validPwd || !validMatch}
+                                >
+                                    Зарегистрировать
+                                </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             )}
         </>
